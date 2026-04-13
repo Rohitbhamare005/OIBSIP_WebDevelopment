43d5ff8d83010584c4b0417a6d0d355f45173c48
Rohitbhamare005 <rohitsbhamare05@gmail.com>
2026-04-14 00:51:54 +0530

    Images sliding fix

--- Task2_TributePage/script.js
+++ Task2_TributePage/script.js
@@ -1,11 +1,23 @@
+// SLIDER
 let slides = document.querySelectorAll(".slide");
 let index = 0;
 
 function showSlide() {
-    slides.forEach((slide) => slide.classList.remove("active"));
+    slides.forEach(slide => slide.classList.remove("active"));
     slides[index].classList.add("active");
-
     index = (index + 1) % slides.length;
 }
 
-setInterval(showSlide, 3000); // change every 3 sec
+setInterval(showSlide, 3000);
+
+// SCROLL ANIMATION
+let sections = document.querySelectorAll(".section");
+
+window.addEventListener("scroll", () => {
+    sections.forEach(section => {
+        let top = section.getBoundingClientRect().top;
+        if (top < window.innerHeight - 50) {
+            section.classList.add("show");
+        }
+    });
+});
